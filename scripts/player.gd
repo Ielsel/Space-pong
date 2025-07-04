6c01d529e21631491c64fb50ae5b037f28b225b2
Leslei Lima <leslei.lima@gmail.com>
2025-07-04 11:25:06 -0300

    Controls changed

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,8 +10,8 @@ func _physics_process(delta: float) -> void:
 	velocity.x = 0
 	
 	if ball.started == true:
-		if Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("Left"):
 			velocity.x -= speed
-		if Input.is_action_pressed("ui_right"):
+		if Input.is_action_pressed("Right"):
 			velocity.x += speed
 	move_and_collide(velocity * delta)
